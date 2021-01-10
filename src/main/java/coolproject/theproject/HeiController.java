1af661df4e335d6a6237f24bfc895e8a9d1e2771
andybobandy <43346364+andybobandy@users.noreply.github.com>
2021-01-10 11:24:03 +0100

    added first controller

---
+++ src/main/java/coolproject/theproject/HeiController.java
@@ -0,0 +1,12 @@
+package coolproject.theproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeiController {
+    @GetMapping("/")
+    public String hei(String navn){
+        return "Hei verden " + navn;
+    }
+}
