0f4c8903c7eb5242bd6f3b1af866052810ab3fe5
andybobandy <43346364+andybobandy@users.noreply.github.com>
2021-01-10 11:05:26 +0100

    Commit project

---
+++ src/main/java/coolproject/theproject/TheprojectApplication.java
@@ -0,0 +1,13 @@
+package coolproject.theproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheprojectApplication.class, args);
+    }
+
+}
